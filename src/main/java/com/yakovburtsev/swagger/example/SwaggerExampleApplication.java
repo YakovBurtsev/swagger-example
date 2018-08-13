c84ff548d126971a2b5bcd3662f7bea59cd146b5
yburtsev <yakovburtsev@gmail.com>
2018-08-13 09:36:14 +0300

    first commit

---
+++ src/main/java/com/yakovburtsev/swagger/example/SwaggerExampleApplication.java
@@ -0,0 +1,7 @@
+package com.yakovburtsev.swagger.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerExampleApplication {
+}
